2f0c0596090068bbbc6a7a8fc012b06c88a029fa
fruitsaladisland <andrewkchan.akc@gmail.com>
2017-01-22 19:23:01 +0000

    AutoCommit: Sun Jan 22 19:23:01 UTC 2017

--- src/actions/ScopeActions.js
+++ src/actions/ScopeActions.js
@@ -47,7 +47,7 @@ export function fetchDiagnosisSuggestions(capturedImage) {
             type: 'image/jpeg',
             name: 'photo.jpg',
         };
-        const UPLOAD_URL = "http://107.170.241.119:8080";
+        const UPLOAD_URL = "https://107.170.241.119:8080";
         const body = new FormData()
         body.append('file', photo)
         
