9b1952f4783febffa99f0d60f94196652200e8cb
fruitsaladisland <andrewkchan.akc@gmail.com>
2017-01-22 03:51:22 +0000

    AutoCommit: Sun Jan 22 03:51:22 UTC 2017

--- android/app/src/main/java/com/farmr/Guesser.java
+++ android/app/src/main/java/com/farmr/Guesser.java
@@ -18,7 +18,7 @@ public class Guesser{
     
     static{
         try {
-            singleton = Classifier.create("AssetMangerPlaceholder",MODEL_FILE,LABEL_FILE,NUM_CLASSES,INPUT_SIZE,IMAGE_MEAN,IMAGE_STD,INPUT_NAME,OUTPUT_NAME);
+            singleton = TensorFlowImageClassifier.create("AssetMangerPlaceholder",MODEL_FILE,LABEL_FILE,NUM_CLASSES,INPUT_SIZE,IMAGE_MEAN,IMAGE_STD,INPUT_NAME,OUTPUT_NAME);
         } catch (final Exception e) {
             int i = 1+1;
         }
