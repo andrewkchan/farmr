4247542a3314c817df5c3db077f67e35c3326d68
fruitsaladisland <andrewkchan.akc@gmail.com>
2017-01-22 07:58:32 +0000

    AutoCommit: Sun Jan 22 07:58:32 UTC 2017

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -60,7 +60,7 @@ class Dashboard extends Component {
         for (let i = 0; i < cropIds.length; i += MAX_ITEMS_PER_ROW) {
             const cropRow = cropIds.slice(i, i + MAX_ITEMS_PER_ROW).map((cropId) => {
                 return (
-                    <View style={{flex: 1, flexDirection: "column", alignSelf: "center", margin: 10}}>
+                    <View style={{flex: 1, flexDirection: "column", alignSelf: "center", margin: 10, marginLeft: 5}}>
                         <Image
                             source={require("../images/dirticon.png")}
                             style={thumbnailStyle}
