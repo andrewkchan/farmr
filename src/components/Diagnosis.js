7426216c49b7f5de512e60a01601975de5d5dea0
fruitsaladisland <andrewkchan.akc@gmail.com>
2017-01-22 04:01:41 +0000

    AutoCommit: Sun Jan 22 04:01:41 UTC 2017

--- src/components/Diagnosis.js
+++ src/components/Diagnosis.js
@@ -22,14 +22,19 @@ const styles = StyleSheet.create({
     }
 })
 
-const onButtonPress = () => {
-     const { navigator } = this.props;
+
+
+class Diagnosis extends Component {
+    constructor(props){
+        super(props);
+        this.onButtonPress = this.onButtonPress.bind(this);
+    }
+    onButtonPress(){
+        const { navigator } = this.props;
         navigator.replace({
             id: "dashboard"
         });
-};
-
-class Diagnosis extends Component {
+    }
     render() {
         return (
                 <View style={containerStyle}>
