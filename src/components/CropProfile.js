0906bb6f2093acdb9321ce245f1ec1b493094a2c
fruitsaladisland <andrewkchan.akc@gmail.com>
2017-01-22 10:18:49 +0000

    AutoCommit: Sun Jan 22 10:18:49 UTC 2017

--- src/components/CropProfile.js
+++ src/components/CropProfile.js
@@ -51,7 +51,7 @@ class CropProfile extends Component {
                         <Icon
                             name = "arrow-circle-left"
                             size={50}
-                            style = {{alignSelf:'center', marginTop: 400}}
+                            style = {{alignSelf:'center', marginTop: 300}}
                             color="#499E9C"
                         />
                 </TouchableOpacity>
