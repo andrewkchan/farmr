b06d3cef95fb530a93b38fde5ce77f4bc4d82914
fruitsaladisland <andrewkchan.akc@gmail.com>
2017-01-22 08:05:35 +0000

    AutoCommit: Sun Jan 22 08:05:35 UTC 2017

--- src/constants/CropTypes.js
+++ src/constants/CropTypes.js
@@ -4,3 +4,47 @@ export const CHERRY = "CHERRY";
 export const CORN = "CORN";
 export const GRAPE = "GRAPE";
 export const ORANGE = "ORANGE";
+export default cropTypes = {
+    APPLE: {
+        name: "Apple"
+    },
+    BLUEBERRY: {
+        name: "Blueberry"
+    },
+    CHERRY: {
+        name: "Cherry"
+    },
+    CORN: {
+        name: "Corn"
+    },
+    GRAPE: {
+        name: "Grape"
+    },
+    ORANGE: {
+        name: "Orange"
+    },
+    PEACH: {
+        name: "Peach"
+    },
+    PEPPER: {
+        name: "Pepper"
+    },
+    POTATO: {
+        name: "Potato"
+    },
+    RASPBERRY: {
+        name: "Raspberry"
+    },
+    SOYBEAN: {
+        
+    },
+    SQUASH: {
+        
+    },
+    STRAWBERRY: {
+        
+    },
+    TOMATO: {
+        
+    }
+}
