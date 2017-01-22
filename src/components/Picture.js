f2be90ba31059c8e356079032d5f17612441014e
fruitsaladisland <andrewkchan.akc@gmail.com>
2017-01-22 08:42:16 +0000

    AutoCommit: Sun Jan 22 08:42:16 UTC 2017

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -3,6 +3,8 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import IconI from "react-native-vector-icons/Ionicons";
 import PictureBottomBar from "../components/PictureBottomBar";
 import PictureTopBar from "../components/PictureTopBar";
+import ImagePicker from 'react-native-image-crop-picker';
+
 
 import { View, Text, Image, PixelRatio } from "react-native";
 
